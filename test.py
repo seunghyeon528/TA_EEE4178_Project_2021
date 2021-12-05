34d16156ce4cc6659260877f5acd95f014ae37fc
seunghyeon528 <77431192+seunghyeon528@users.noreply.github.com>
2021-12-05 12:08:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -81,6 +81,11 @@ with torch.no_grad():
         outputs = model_test(images)
         _,predicted = torch.max(outputs.data,1)
         correct += (predicted==labels).sum()
-        
+
+    # batch size 가 7800의 양수라면 아래 방식으로 해도 정확
+    print('Accuracy of the last_model network on the {} test images: {} %'.\
+          format(len(test_loader)*batch_size, 100 * correct / (len(test_loader)*batch_size)))
+            
+    # batch size 가 7800 의 양수가 아니라면 아래 방식으로 해야만 정확한 accuracy 얻을 수 있음. 
     print('Accuracy of the last_model network on the {} test images: {} %'.\
           format(len(test_data), 100 * correct / (len(test_data))))
